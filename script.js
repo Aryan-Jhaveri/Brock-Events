20eca4d12543917e0882e80c4608ce31fd076528
Aryan Jhaveri <96702278+Aryan-Jhaveri@users.noreply.github.com>
2024-02-18 13:50:01 -0500

    idk

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ document.addEventListener('DOMContentLoaded', function () {
     return events;
 }
 
-function displayEventsInTable(events) {
+  function displayEventsInTable(events) {
     const eventsBody = document.getElementById('events-body');
     if (eventsBody) {
         eventsBody.innerHTML = ''; // Clear previous events
